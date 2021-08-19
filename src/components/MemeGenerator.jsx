d98853127eac8309384d1c54c1801c54c025638e
suqui <lisacatlaw@gmail.com>
2021-08-19 18:33:31 +0200

    basic generator build

--- src/components/MemeGenerator.jsx
+++ src/components/MemeGenerator.jsx
@@ -1,5 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 
 export const MemeGenerator = () => {
-    return <h1>My Generator</h1>
+    const [ topText, bottomText, randomMeme ] = useState();
+
+    const handleChange = () => {
+        return "hello world";
+    }
+
+    return (
+        <div className="form">
+            <form>
+                <input 
+                    name="topText"
+                    value={topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    name="bottomText"
+                    value={bottomText}
+                    onChange={handleChange}
+                />
+            </form>
+            <br />
+
+            <img
+                src=""
+                alt="random meme"
+                value={randomMeme}
+                name="randomMeme"
+            />
+            <br />
+        </div>
+    )
 }
